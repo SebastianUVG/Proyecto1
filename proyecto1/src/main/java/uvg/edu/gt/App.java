854ac69ce036816428a16d79215d3c8d9300859b
SebastianUVG <gar22291@uvg.edu.gt>
2024-03-21 17:17:19 -0600

    Para leer archivos
    Funcino que hara que se pueda leer los archivos de texto y asi empezar a programar en base a los codigos de lisp que ingresen

--- proyecto1/src/main/java/uvg/edu/gt/App.java
+++ proyecto1/src/main/java/uvg/edu/gt/App.java
@@ -1,13 +1,53 @@
 package uvg.edu.gt;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+
+    public static String[] textReader(String rutaArchivo) throws Exception {
+        
+
+        List<String> ListaPalabras = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))){
+            String line ;
+            while ((line = br.readLine()) != null) {
+                String[] wordsInLine = line.split("\\s+"); // Dividir la línea en palabras usando espacios como delimitador
+                for (String word : wordsInLine) {
+                    ListaPalabras.add(word);
+                }
+            }
+        }
+       
+
+        String[] PalabrasLista = new String[ListaPalabras.size()];
+        return ListaPalabras.toArray(PalabrasLista);
+    
+    }
+
+
+
+
+
+
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        String rutaArchivo = "C:\\Users\\sebas\\OneDrive\\Escritorio\\Github\\EstructuraDeDatos\\Proyecto1\\proyecto1\\src\\main\\java\\uvg\\edu\\gt\\archivo.txt";
+        try {
+            String[] palabras  = textReader(rutaArchivo);
+            System.out.println("Palabras en el archivo: ");
+            for (String palabra : palabras){
+                System.out.println(palabra);
+            }
+        }catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
 }
