faf832410a4226b6eb3bd0a909cccad151895f9a
SebastianUVG <gar22291@uvg.edu.gt>
2024-03-21 18:01:23 -0600

    Imprimir lo que se va a evaluar
    Imprimir como se muestra en el archivo lo que se quiere evaluar

--- proyecto1/src/main/java/uvg/edu/gt/App.java
+++ proyecto1/src/main/java/uvg/edu/gt/App.java
@@ -13,8 +13,6 @@ public class App
 {
 
     public static String[] textReader(String rutaArchivo) throws Exception {
-        
-
         List<String> ListaPalabras = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))){
             String line ;
@@ -25,27 +23,33 @@ public class App
                 }
             }
         }
-       
-
         String[] PalabrasLista = new String[ListaPalabras.size()];
         return ListaPalabras.toArray(PalabrasLista);
     
     }
 
+    public static void leerArchivo(String rutaArchivo) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
 
+            }
 
-
-
+        }
+    }
 
     public static void main( String[] args ) throws Exception
     {
         String rutaArchivo = "C:\\Users\\sebas\\OneDrive\\Escritorio\\Github\\EstructuraDeDatos\\Proyecto1\\proyecto1\\src\\main\\java\\uvg\\edu\\gt\\archivo.txt";
         try {
             String[] palabras  = textReader(rutaArchivo);
+            System.out.println(palabras);
             System.out.println("Palabras en el archivo: ");
             for (String palabra : palabras){
                 System.out.println(palabra);
             }
+            leerArchivo(rutaArchivo);
         }catch (IOException e) {
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
